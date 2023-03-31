d64da0a04d9a67b30bf7f26ad329a14c6ce5517f
Ahmad Wildan <ahmadwildan.id@gmail.com>
2023-03-31 18:46:55 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import { getmovie, searchmovie } from "./api";
+import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [popular, setpopular] = useState([]);
+
+  useEffect(() => {
+    getmovie().then((result) => {
+      setpopular(result);
+    });
+  }, []);
+
+  const search = async (q) => {
+    if (q.length > 3) {
+      const query = await searchmovie(q);
+      setpopular(query.results);
+    }
+  };
+
+  const Popularmovie = () => {
+    return popular.map((movie, i) => {
+      return (
+        <div className="movie-wrapper" key={i}>
+          <div className="movie-title">{movie.title}</div>
+          <img
+            className="movie-image"
+            src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
+            alt={movie.title}
+          />
+          <div className="movie-date">Release : {movie.release_date}</div>
+          <div className="movie-rate">Rate : {movie.vote_average}</div>
+        </div>
+      );
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>CARI FILM</h1>
+        <input
+          placeholder="Ketik Judul..."
+          className="search"
+          onChange={({ target }) => search(target.value)}
+        />
+        <div className="movie-container">
+          <Popularmovie />
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
